c1c5e6c787628115869400985b66d3bf64d0dff4
Chris Wacker <chriswacker228@gmail.com>
2026-01-01 19:06:04 -0800

    updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 -- Saving, quitting
 vim.keymap.set({'n', 'x'}, '<leader>w', '<cmd>w<cr>')
-vim.keymap.set({'n', 'x'}, '<leader>W', '<cmd>w<cr>')
+vim.keymap.set({'n', 'x'}, '<leader>W', '<cmd>wa<cr>')
 vim.keymap.set({'n', 'x'}, '<leader>q', '<cmd>q<cr>')
 vim.keymap.set({'n', 'x'}, '<leader>Q', '<cmd>qa<cr>')
 
