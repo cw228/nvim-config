446b3e3fc322ed3c70e2d88862b36a7736c5c86e
Chris Wacker <chriswacker228@gmail.com>
2026-01-10 14:58:34 -0800

    cleanup

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -229,18 +229,5 @@ return {
             }
         }
     },
-    -- {
-    --     'akinsho/bufferline.nvim',
-    --     version = "*",
-    --     dependencies = 'nvim-tree/nvim-web-devicons',
-    --     config = function()
-    --         local bufferline = require('bufferline')
-    --         bufferline.setup({
-    --             options = {
-    --                 style_preset = bufferline.style_preset.no_italic,
-    --             },
-    --         })
-    --     end
-    -- }
 }
 
