ed6388d36bc21a8c8f2b691016fb662f19cf5c4a
cw228 <chriswacker228@gmail.com>
2026-02-02 22:36:03 -0800

    better signature help border

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -229,6 +229,9 @@ return {
         "ray-x/lsp_signature.nvim",
         event = "InsertEnter",
         opts = {
+            handler_opts = {
+                border = { "", "", "", " ", "", "", "", " " },
+            },
         },
     }
 }
