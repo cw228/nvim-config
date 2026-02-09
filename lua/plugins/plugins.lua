1a7a6cb8510a767cd4c910b4b95e09a3ddd4e4f1
Chris Wacker <chriswacker228@gmail.com>
2026-02-09 14:42:28 -0800

    add cmp move prev item

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -119,6 +119,7 @@ return {
                     ['<C-e>'] = cmp.mapping.abort(),
                     ['<CR>'] = cmp.mapping.confirm({ select = true }),
                     ['<Tab>'] = cmp.mapping.select_next_item(),
+                    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
                 }),
                 sources = cmp.config.sources(
                     {
